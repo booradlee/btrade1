621a7c44350279873ceb5dd5aac7cf605891583f
booradlee <35990805+booradlee@users.noreply.github.com>
2018-02-14 03:52:51 -0600

    Update BinanceBotApplication.java

--- src/main/java/io/github/unterstein/BinanceBotApplication.java
+++ src/main/java/io/github/unterstein/BinanceBotApplication.java
@@ -31,7 +31,7 @@ public class BinanceBotApplication {
   private double tradeProfit;
 
   @Value("${TRADE_AMOUNT:150}")
-  private int tradeAmount;
+  private double tradeAmount;
 
   @Value("${BASE_CURRENCY:ETH}")
   private String baseCurrency;
